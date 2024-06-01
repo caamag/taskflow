cfcb6cd8e1acfdeaa2345e07bd02daae97790d79
Caio Lopes <caiolopesfv@gmail.com>
2024-06-01 17:05:19 -0300

    enviando atualizações

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+//context
+import { AuthProvider } from './context/authContext';
+import { onAuthStateChanged } from 'firebase/auth';
 
 //pages
 import Home from './pages/home/home';
-import Dashboards from './pages/dashboards/dashboard';
+import Dashboards from './pages/dashboards/dashboards';
+import Login from './pages/login/login';
 
 //components
 import Nav from './components/nav/nav';
+
+//hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthenticate'
 
 function App() {
 
@@ -14,6 +23,7 @@ function App() {
     <BrowserRouter>
       <Nav />
       <Routes>
+        <Route path='/login' element={<Login />} ></Route>
         <Route path='/' element={<Home />}></Route>
         <Route path='/dashboards' element={<Dashboards />}></Route>
       </Routes>
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,12 @@
+
+import './login.css'
+
+const Login = () => {
+
+    return <div className='login-container'>
+        <h1>Página de login</h1>
+
+    </div>
+}
+
+export default Login;
