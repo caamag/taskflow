bc0ddc274436c2ae5e1da05c7c530470619c0778
Caio Lopes <caiolopesfv@gmail.com>
2024-07-13 17:43:20 -0300

    Enviando atualizações

--- src/pages/customers/customers.jsx
+++ src/pages/customers/customers.jsx
@@ -1,7 +1,26 @@
 
 import './customers.css'
+import { useAuthValue } from '../../context/authContext'
+import { useState, useEffect, useId } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../../firebase/config'
 
 const Customers = () => {
+
+    const [customers, setCustomers] = useState([])
+    const { user } = useAuthValue()
+
+    useEffect(() => {
+        const getCustomers = async () => {
+            const userId = user.uid;
+            const querySnapshot = await getDocs(collection(db, 'users', userId, 'Clientes'))
+            const customerList = querySnapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }))
+            setCustomers(customerList)
+        }
+    }, [])
 
     return <div className='container cusutomer-container'>
 
