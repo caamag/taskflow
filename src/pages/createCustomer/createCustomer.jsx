03af8e9b85eb53b15e1d872e17d8d917cf9b8279
Caio Lopes <caiolopesfv@gmail.com>
2024-07-12 21:41:37 -0300

    Enviando atualizações

--- src/pages/createCustomer/createCustomer.jsx
+++ src/pages/createCustomer/createCustomer.jsx
@@ -1,9 +1,33 @@
 import './createCustomer.css'
 
+import { useEffect, useState } from 'react'
+import { db } from '../../firebase/config'
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
+import { useAuthValue } from '../../context/authContext'
+
 const CreateCustomer = () => {
 
-    return <div className='container create-user-container'>
+    const [name, setName] = useState('Cainho')
+    const [email, setEmail] = useState('cainho@teste.com')
+    const { user } = useAuthValue();
 
+    const addCustomer = async () => {
+        if (user) {
+            const userId = user.uid;
+            await addDoc(collection(db, 'users', userId, 'Clientes'), {
+                name,
+                email,
+                createdAt: serverTimestamp(),
+            }).then(response => {
+                console.log(response);
+            })
+        } else {
+            console.log('User logout now.');
+        }
+    }
+
+    return <div className='container create-user-container'>
+        <button onClick={() => { addCustomer() }}>criar cliente</button>
     </div>
 }
 
