cf75dbb070684721b4a842ca242dc95e88e0f2c2
Caio Lopes <caiolopesfv@gmail.com>
2024-07-29 22:20:09 -0300

    Finalizando criação de projeto

--- src/pages/newProject/newProject.jsx
+++ src/pages/newProject/newProject.jsx
@@ -1,9 +1,9 @@
 
 import './newProject.css'
-import searchIcon from '../customers/assets/search.png'
 import loadingIcon from '../../../public/loading.jpg'
 
 import { db } from '../../firebase/config'
+import { collection, getDocs, addDoc, serverTimestamp } from 'firebase/firestore'
 import { useState, useEffect } from 'react'
 import { useAuthValue } from '../../context/authContext'
 import PopupError from '../../components/popupError/popup'
@@ -13,46 +13,119 @@ import Subtitle from '../../components/subtitle/subtitle'
 
 const NewProject = () => {
 
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
+    const [success, setSuccess] = useState(false)
+    const [customers, serCustomers] = useState([])
+
     const [projectName, setProjectName] = useState('')
-    const [price, setPrice] = useState(null)
+    const [price, setPrice] = useState('')
     const [details, setDetails] = useState('')
+    const [customerSelected, setCustomerSelected] = useState('')
 
-    console.log(price);
+    const { user } = useAuthValue()
+    const userId = user.uid
 
-    return <div className='container new-project-container'>
-        <Subtitle
-            subtitle={'Adicionar novo projeto'}
-            description={'Crie novos projetos para melhorar a organização do seu trabalho.'}
-        />
+    useEffect(() => {
+        const getCustomers = async () => {
+            setLoading(true)
+            const querySnapshot = await getDocs(collection(db, 'users', userId, 'Clientes'))
+            const customerList = querySnapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }))
+            customerList.sort((a, b) => b.createdAt - a.createdAt)
+            serCustomers(customerList)
+            setLoading(false)
+        }
+        getCustomers()
+    }, [])
 
-        <form>
-            <input
-                type="text"
-                placeholder='Título do projeto:'
-                value={projectName}
-                onChange={(e) => { setProjectName(e.target.value) }}
-                required
+    const handleCreateProject = async (e) => {
+        e.preventDefault();
+
+        if (customerSelected === '' || customerSelected === '-') {
+            setError(true)
+            setErrorMessage('Selecione um cliente.')
+            setTimeout(() => { setError(false) }, 5000)
+            return;
+        }
+
+        try {
+            setLoading(true)
+            addDoc(collection(db, 'users', userId, 'Projetos'), {
+                projectName,
+                price: Number(price),
+                details,
+                customerSelected,
+                createdAt: serverTimestamp(),
+            })
+
+            setSuccess(true)
+            setTimeout(() => { setSuccess(false) }, 5000)
+            setLoading(false)
+            setProjectName('')
+            setPrice('')
+            setDetails('')
+            setCustomerSelected('')
+        } catch (error) {
+            setError(true)
+            setErrorMessage('Erro ao criar projeto.')
+            setLoading(false)
+            setTimeout(() => { setError(false) }, 5000)
+        }
+    }
+
+    return <>
+        {!loading && <div className='container new-project-container'>
+            <Subtitle
+                subtitle={'Adicionar novo projeto'}
+                description={'Crie novos projetos para melhorar a organização do seu trabalho.'}
             />
 
-            <input
-                type="number"
-                placeholder='Preço:'
-                value={price}
-                onChange={(e) => { setPrice(e.target.value) }}
-                required
-            />
+            <form onSubmit={handleCreateProject}>
+                <input
+                    type="text"
+                    placeholder='Título do projeto:'
+                    value={projectName}
+                    onChange={(e) => { setProjectName(e.target.value) }}
+                    required
+                />
 
-            <textarea
-                placeholder='Detalhes do projeto (opicional):'>
-            </textarea>
+                <input
+                    type="number"
+                    placeholder='Preço:'
+                    value={price}
+                    onChange={(e) => { setPrice(e.target.value) }}
+                    required
+                />
 
-            <select>
-                <option value="-">-</option>
-                <option value="Caio">Caio</option>
-                <option value="Caio">Caio</option>
-            </select>
-        </form>
-    </div>
+                <textarea
+                    placeholder='Detalhes do projeto (opicional):'
+                    value={details}
+                    onChange={(e) => { setDetails(e.target.value) }}
+                ></textarea>
+
+                <select value={customerSelected} onChange={(e) => { setCustomerSelected(e.target.value) }}>
+                    <option>-</option>
+                    {customers.map(customer => (
+                        <option
+                            value={customer.name}
+                            key={customer.id}>
+                            {customer.name}
+                        </option>
+                    ))}
+                </select>
+
+                <button type='submit'>Criar projeto</button>
+            </form>
+        </div>}
+
+        {loading && <img className='loading-initial' src={loadingIcon} />}
+        {error && <PopupError errorMessage={errorMessage} />}
+        {success && <PopupSuccess successMessage={'Projeto criado com sucesso.'} />}
+    </>
 }
 
 export default NewProject;
